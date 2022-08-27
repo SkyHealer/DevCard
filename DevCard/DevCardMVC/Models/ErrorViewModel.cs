67abd75e97c932344ac05f3929707fb33deb6d32
hossein rahmian <hosen1612@yahoo.com>
2022-08-27 11:36:16 +0430

    DevCard Initialize

---
+++ DevCard/DevCardMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevCardMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
