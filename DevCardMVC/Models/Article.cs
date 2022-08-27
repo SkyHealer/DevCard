b487e416ef0c0f89dae2e579a26a267e2f8a1bbe
hossein rahmian <hosen1612@yahoo.com>
2022-08-27 15:33:05 +0430

    projects section from index page moved to partial

---
+++ DevCardMVC/Models/Article.cs
@@ -0,0 +1,8 @@
+﻿namespace DevCardMVC.Models;
+
+public class Article
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
---
+++ DevCardMVC/Models/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace DevCardMVC.Models
+{
+	public class Project
+	{
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+}
